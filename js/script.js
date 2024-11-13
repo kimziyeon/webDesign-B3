75cf07116e30eb8e6f7b49d378728c86fe421f0a
kimziyeon <aa40254037@gmail.com>
2024-11-13 18:11:54 +0900

    20241114 재연습

--- js/script.js
+++ js/script.js
@@ -1,48 +1,47 @@
 $(function () {
 
-    $('header>.inner>ul>li').mouseenter(function () {
-        var i = $(this).index();
-        console.log(i);
-        $('.sub').hide();
-        $('.sub').eq(i).stop().slideDown();
+
+    // 네브
+    $('nav>ul>li').mouseenter(function () {
+        $(this).children('.sub').stop().slideDown();
+    })
+    $('nav>ul>li').mouseleave(function () {
+        $(this).children('.sub').stop().slideUp();
     })
 
-    $('header>.inner>ul>li').mouseleave(function () {
-        $('.sub').stop().slideUp();
+    $('nav>ul>li').focusin(function () {
+        $(this).children('.sub').stop().slideDown();
+        $(this).children('a').addClass('on');
+    })
+    $('nav>ul>li').focusout(function () {
+        $(this).children('.sub').stop().slideUp();
+        $(this).children('a').removeClass('on');
+    })
+    $('.sub>li').focusin(function () {
+        $(this).children('a').addClass('on');
+    })
+    $('.sub>li').focusout(function () {
+        $(this).children('a').removeClass('on');
     })
 
+
+
+    // 슬라이드
     function slide() {
-        $('.slide ul').animate({ top: '-300px' }, 1000, function () {
-            $('.slide ul').append(
-                $('.slide ul li').first());
-            $('.slide ul').css({ top: '0' });
+        $('.slide>ul').animate({ top: '-300px' }, 1000, function () {
+            $(this).append($('.slide>ul>li').first())
+            $('.slide>ul').css({ top: '0' });
         })
     }
     setInterval(slide, 3000)
 
 
-    $('.popup_on').click(function () {
-        $('.popup').show();
+    // 팝업
+    $('.pop_on').click(function () {
+        $('.pop').show();
     })
-
-    $('.popup_off').click(function () {
-        $('.popup').hide();
+    $('.pop_off').click(function () {
+        $('.pop').hide();
     })
-
-
-    $('header .inner>ul>li').focusin(function () {
-        var i = $(this).index();
-
-        $('.sub').hide();
-
-
-        $('.sub').eq(i).stop().show();
-
-    })
-
-    $('header .inner>ul>li:last-child>.sub>li:last-child').focusout(function () {
-        $('.sub').stop().hide();
-    })
-
 
 })
